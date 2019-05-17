864119c3653869d1b1de0fac71486524ef221b3c
icarosanthy <icaro_neves1998@hotmail.com>
2019-05-17 17:33:25 -0300

    meu projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RelatorioComponent } from './relatorio/relatorio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RelatorioComponent
   ],
   imports: [
     BrowserModule,
